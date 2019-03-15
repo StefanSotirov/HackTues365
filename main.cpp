5636dbcc0e7e3d05dc640d7e24124d456bdbb0a2
blueneon96 <ssotirov14@gmail.com>
2019-03-15 11:57:51 +0200

    Ready for work

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
-#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <stdlib.h>
+#include <ctime>
+#include <windows.h>
 
 using namespace std;
 
+void percentChance(int outcome1Chance, int outcome2Chance, void (*outcome1)(), void (*outcome2)())
+{
+    srand (time(0));
+    int a = 1 + (rand() % 100);
+    if (a <= outcome1Chance)
+    {
+        outcome1();
+    }
+    else
+    {
+        outcome2();
+    }
+}
+
+void Time(bool ifTrue)
+{
+    int seconds = 0;
+    int year = 0;
+    while (ifTrue == false)
+    {
+        year++;
+        Sleep(1000);
+    }
+}
+
 class Ruler
+{
+
+};
+
+class Children
 {
 
 };
@@ -14,8 +48,17 @@ class Relationships
 
 class Resourses
 {
-
+    public:
+    int money;
+    long people;
+    long army;
+    int food;
+    void change_money();
+    void change_people();
+    void change_army();
+    void change_food();
 };
+
 int main()
 {
 
